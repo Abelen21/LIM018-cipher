ebf01af5ecc3cbb8d5f97c3a625be31ae24a284d
unknown <Beln>
2022-05-26 06:02:32 -0500

    asociado botón cifrar con función cifrar

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import cipher from './cipher.js';
 
+const inputTexto = document.querySelector('#texto');
+const inputClave = document.querySelector("#clave");
+const resultado = document.querySelector("#resultado");
+
+
+const btncifrar = document.querySelector('#cifrar');
+btncifrar.addEventListener('click',cifrar());
+
+ 
+function cifrar(){
+    const textoCifrado = cipher.encode(String(inputTexto.value), parseInt(inputClave.value));
+    resultado.value = textoCifrado;
+}
+
+
+
 console.log(cipher);
