03e26011a1fcbfc5995f616c3bc558625900c950
unknown <Beln>
2022-05-26 06:01:30 -0500

    fuagregado condicional a funcion cifrado

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,21 @@
 const cipher = {
-  // ...
+   // ...
+  encode: function( claveParaCifrar,textoParaCifrar){
+
+    if(claveParaCifrar!=0 && textoParaCifrar!= " " && claveParaCifrar!='' && textoParaCifrar!=""){
+      for ( let i = 0; i < textoParaCifrar.length; i++){
+        textoParaCifrar = textoParaCifrar.split("");
+        textoParaCifrar[i] = String.fromCharCode((textoParaCifrar.charCodeAt(i)-65 + claveParaCifrar)%26 +65);
+        textoParaCifrar = textoParaCifrar.join("");
+      }
+      return textoParaCifrar;
+    }
+    else{
+      console.toThrow(TypeError);
+    }
+  }
+ 
 };
 
 export default cipher;
+
