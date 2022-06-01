b3437b3ec0a6ef73c7590e619b3c0ed3c880081b
unknown <Beln>
2022-06-01 09:52:01 -0500

    terminada pantalla 1 presentación

--- src/index.js
+++ src/index.js
@@ -5,10 +5,14 @@ const inputClave = document.querySelector("#clave");
 const resultado = document.querySelector("#resultado");
 const btncifrar = document.querySelector('#cifrar');
 const btndescifrar = document.querySelector('#descifrar');
+// const resultado1 = document.querySelector('.resultado1');
+const resultado2 = document.querySelector('.resultado2');
+
 
 
 btncifrar.addEventListener('click',()=>{ 
     const textoCifrado = cipher.encode(parseInt(inputClave.value),inputTexto.value);
+    resultado2.style.visibility = "visible";
     resultado.value = textoCifrado;
     inputTexto.value = '';
     inputClave.value = ''} )
