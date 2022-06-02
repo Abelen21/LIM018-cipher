212b488e0aa70710799a3307f1b292fb7fe23dec
unknown <Beln>
2022-06-02 11:58:19 -0500

    Updates

--- index.js
+++ index.js
@@ -5,26 +5,27 @@ const inputClave = document.querySelector("#clave");
 const resultado = document.querySelector("#resultado");
 const btncifrar = document.querySelector('#cifrar');
 const btndescifrar = document.querySelector('#descifrar');
+const resultado2 = document.querySelector('.resultado2');
 
 
 btncifrar.addEventListener('click',()=>{ 
     const textoCifrado = cipher.encode(parseInt(inputClave.value),inputTexto.value);
+    resultado2.style.visibility = "visible";
     resultado.value = textoCifrado;
     inputTexto.value = '';
     inputClave.value = ''} )
 
 btndescifrar.addEventListener('click',()=>{
     const textoDescifrado = cipher.decode(parseInt(inputClave.value),inputTexto.value);
+    resultado2.style.visibility = "visible";
     resultado.value = textoDescifrado;
     inputTexto.value = '';
     inputClave.value = '';
 } )
 
 
-/* function btncifrar(){
-    const textoCifrado = cipher.encode((inputTexto.value), parseInt(inputClave.value));
-    resultado.value = textoCifrado;
-} */
+
+
 
 
 // console.log(cipher);
